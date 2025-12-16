d63636ddfc8f77485780568a0c339f75cbb6e7ec
Gabriela Oliveira <gabriela.gomestrab@gmail.com>
2025-12-16 17:32:24 -0300

    Moving pieces

--- src/main/java/com/chesssystem/ChessSystemApplication.java
+++ src/main/java/com/chesssystem/ChessSystemApplication.java
@@ -1,10 +1,12 @@
 package com.chesssystem;
 
-import boardgame.Board;
-import boardgame.Position;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class ChessSystemApplication {
@@ -12,9 +14,20 @@ public class ChessSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(ChessSystemApplication.class, args);
 
+        Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
 
-        UI.printBoard(chessMatch.getPieces());
-    }
+        while (true){
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
 
+            System.out.println();
+            System.out.print("Target");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
+    }
 }
